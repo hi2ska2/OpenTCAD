5964a2432c8e535db53b5ac4cb1a108d0737c7ba
Sung-Min Hong <smhong@gist.ac.kr>
2026-02-05 12:49:45 +0900

    Introducing CMakeLists.txt

--- SRC/main.cc
+++ SRC/main.cc
@@ -1,11 +1,10 @@
 // main.cc
 // Created     : 2024. 11. 06.
-// Last updated: 2024. 11. 06.
+// Last updated: 2026. 02. 05.
 
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-  std::cout << "Hello, world!\n";
   return 0;
 }
